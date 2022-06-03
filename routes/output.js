1211b6e2a65eb8f163c03a375e942008210cc296
joseyzambranov <joseyzambranovpe@gmail.com>
2022-06-03 17:59:21 -0500

    finished update,delete and consult output

--- routes/output.js
+++ routes/output.js
@@ -14,16 +14,83 @@ router.post("/",verifyToken, (req,res)=>{
  })
 })
 
-/*router.post("/",verifyToken,async(req,res)=>{
-    const newOutput = new Output(req.body)
-    try{
+//UPDATE
 
-        const saveOutput = await newOutput.save()
-        res.status(200).json(saveOutput)
+/*router.put("/:id",verifyToken,async(req,res)=>{
+
+    Output.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+    },{
+        new:true
+    },(err,text)) 
+
+    if(err) return res.status(500).json(err)
+    
+    res.status(200).json(text)
+
+    
+
+})*/
+
+router.put("/:id",verifyToken,async(req,res)=>{
+try{
+    const updateOutput= await Output.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+    },{
+        new:true
+    }) 
+
+    res.status(200).json(updateOutput)
+
+}catch(err){
+    res.status(500).json(err)
+}
+})
+
+//DELETE
+
+router.delete("/:id",verifyToken,(req,res)=>{
+    Output.findByIdAndDelete(req.params.id,(err,text)=>{
+        if(err) return res.status(500).json(err)
+
+        res.status(200).json("Output has been deleted....")
+    })
+})
+
+//GET USER OUTPUT
+
+router.get("/find/:userId",verifyToken,(req,res)=>{
+    Output.find({
+        userId:req.params.userId
+    },(err,text)=>{
+        if(err) return res.status(500).json(err)
+
+        res.status(200).json(text)
+    })
+})
+
+//GET USER OUTPUT TWO FIRST
+
+router.get("/start/:userId",verifyToken,async(req,res)=>{
+
+    try{
+        const output = await Output.find({userId:req.params.userId}).sort({_id:-1}).limit(2)
+        res.status(200).json(output)
+
+
 
     }catch(err){
         res.status(500).json(err)
     }
-})*/
+   
+       /*Output.find({userId:req.params.userId.sort({_id:-1}).limit(2)},(err,text)=>{
+            if(err) return res.status(500).json(err)
+
+            res.status(200).json(text)
+       })*/
+       
+
+  
+})
 
 module.exports = router
